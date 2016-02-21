f5713c158834a0ae70de2de8eb23dd72aa2c24e8
hou80houzhu <hou80houzhu@HOU80HOUZHU>
2016-02-21 21:05:58 +0800

    Merge origin/master
    Conflicts:
    	README.md

--- BROODEROPEN000016/option/root.js
+++
@@ -1,5 +0,0 @@
-/*!
- * @packet option.root; 
- * @include opensite.pc.main:pc;
- * @include opensite.mobi.main:mobi;
- */Option({name:"pc",option:{override:{onendinit:function(){this.addChild({type:"@pc.container"})}}}});
